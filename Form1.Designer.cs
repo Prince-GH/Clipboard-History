a1c1bfac498d6ccb52c0a2baee661686c957a352
prince-gh <pwskills@outlook.com>
2025-01-01 01:11:34 +0530

    Updated clipboad history

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace popup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ClipHistory = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Status = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ClipHistory
+            // 
+            this.ClipHistory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ClipHistory.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ClipHistory.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ClipHistory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ClipHistory.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClipHistory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.ClipHistory.FormattingEnabled = true;
+            this.ClipHistory.ItemHeight = 21;
+            this.ClipHistory.Location = new System.Drawing.Point(0, 0);
+            this.ClipHistory.Margin = new System.Windows.Forms.Padding(10, 20, 10, 20);
+            this.ClipHistory.MinimumSize = new System.Drawing.Size(0, 5);
+            this.ClipHistory.Name = "ClipHistory";
+            this.ClipHistory.Size = new System.Drawing.Size(334, 160);
+            this.ClipHistory.TabIndex = 0;
+            this.ClipHistory.SelectedIndexChanged += new System.EventHandler(this.ClipHistory_SelectedIndexChanged);
+            this.ClipHistory.MouseHover += new System.EventHandler(this.ClipHistory_MouseHover);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.panel1.Controls.Add(this.Status);
+            this.panel1.Controls.Add(this.ClipHistory);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(334, 160);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Status
+            // 
+            this.Status.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Status.AutoSize = true;
+            this.Status.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Status.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Status.Location = new System.Drawing.Point(238, 135);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(0, 19);
+            this.Status.TabIndex = 1;
+            this.Status.Click += new System.EventHandler(this.Status_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(344, 170);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10, 10, 0, 0);
+            this.ShowInTaskbar = false;
+            this.Text = "Clipboard history";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox ClipHistory;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Status;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using System.Data.SQLite;
+
+
+
+namespace popup
+{
+    public partial class Form1 : Form
+    {
+        private IKeyboardMouseEvents _globalHook;
+        private List<string> clipboardHistory = new List<string>();
+        private Timer clipboardTimer;
+        private string connectionString = "Data Source=clipboardData.db;Version=3;";
+
+
+        public Form1()
+        {
+            this.TopMost = true;
+            InitializeComponent();
+            // Call methods on form load
+            //CreateTable();
+            //InsertData("Alice", 25);
+            //ShowData();
+            // Pass the ListBox and the parent Form to the customizer
+            new ClipHistoryCustomizer(ClipHistory, this);
+
+
+
+
+
+
+            // Start the global hook
+            _globalHook = Hook.GlobalEvents();
+            _globalHook.KeyDown += OnGlobalKeyDown;
+
+            // Fetch data form keyboard
+            clipboardTimer = new Timer();
+            clipboardTimer.Interval = 500; // Check every 0.5 sec or 500ms
+            clipboardTimer.Tick += ClipboardTimer_Tick;
+            clipboardTimer.Start();
+
+        }
+
+        //-----------database/////////.................
+
+        // CreateTable: Creates the table if it doesn't exist
+        private void CreateTable()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "CREATE TABLE IF NOT EXISTS ClipDataTable (clipData TEXT)";
+                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // InsertData: Inserts a record into the Users table
+        private void InsertData(string clipData)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "INSERT INTO ClipDataTable (clipData) VALUES (@clipData)";
+                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@clipData", clipData);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // ShowData: Retrieves all records and displays them in MessageBoxes
+        private void ShowData()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM ClipDataTable";
+                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    MessageBox.Show($"ID: {reader["Id"]}, Name: {reader["Name"]}, Age: {reader["Age"]}");
+
+                }
+            }
+        }
+
+
+        //------database end--------------------------
+
+        // Event handler for global key down || press win + O
+        private void OnGlobalKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.O && e.Modifiers == Keys.LWin)
+            {
+                ShowPopUp();
+            }
+        }
+
+        // Add items to the ClipboardHistory list
+        private void ClipboardTimer_Tick(object sender, EventArgs e)
+        {
+            string clipboardText = Clipboard.GetText();
+            if(!string.IsNullOrEmpty(clipboardText) && (clipboardHistory.Count == 0 || !clipboardHistory.Contains(clipboardText)))
+            {
+                clipboardHistory.Add(clipboardText);
+
+                UpdateClipboardHistoryUI();
+            }
+        }
+
+        // Update the lsitBox (ClipHistory)
+        private void UpdateClipboardHistoryUI()
+        {
+            ClipHistory.Items.Clear();
+            foreach (var text in clipboardHistory)
+            {
+                ClipHistory.Items.Add(text);
+            }
+        }
+
+
+        // Show the popUp on the screen when press : win + O
+        private void ShowPopUp()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.TopMost = true;
+            this.Show();
+        }
+
+        // Copy to the clipboard
+        private async void ClipHistory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = ClipHistory.SelectedIndex;
+            if(selectedIndex >= 0)
+                
+            {
+                Clipboard.SetText(clipboardHistory[selectedIndex]);
+
+                // Show the "Copied!" status
+                Status.Text = "Copied!";
+                Status.ForeColor = Color.Green;
+                Status.Visible = true;
+
+                // Start the timer to hide the status after 3 seconds
+                await (Task.Delay(3000));
+                Status.Visible = false;
+
+            }
+        }
+
+        // -----------------------------------------------------------
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            // Clean up the global hook
+            if (_globalHook != null)
+            {
+                _globalHook.KeyDown -= OnGlobalKeyDown;
+                _globalHook.Dispose();
+            }
+            base.OnFormClosed(e);
+        }
+
+        // -----------------------------------------------------------
+
+        private void Status_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ClipHistory_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
